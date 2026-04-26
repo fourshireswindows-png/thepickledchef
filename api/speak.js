fe0a403da0bcdcf4ce6c85ff590de0dfce0062f9
fourshireswindows-png <fourshireswindows@gmail.com>
2026-04-26 19:15:30 +0100

    Update speak.js

--- api/speak.js
+++ api/speak.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         text: clean,
-        model_id: 'eleven_monolingual_v1',
+        model_id: 'eleven_turbo_v2',
         voice_settings: { stability: 0.5, similarity_boost: 0.75 }
       })
     });
